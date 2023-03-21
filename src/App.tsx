a29cb1d0b4f2f02d21fa9d65b5952ad6985209bd
davisuga <daviciencia1@gmail.com>
2023-03-21 11:17:08 -0300

    simply passing the mouse location

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,35 @@ import "./App.css";
 type ZoomOptions = {
   level: number;
 }
+
+type MouseLocation = {
+  x: number;
+  y: number;
+}
+
+function startCapture(displayMediaOptions: DisplayMediaStreamOptions | undefined) {
+  return navigator.mediaDevices
+    .getDisplayMedia(displayMediaOptions)
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+}
+
+
 function App() {
   const [currentFrame, setCurrentFrame] = createSignal<string>()
   const [zoomOptions, setZoomOptions] = createSignal<ZoomOptions>()
-
+  const [mouseLocation, setMouseLocation] = createSignal<MouseLocation>({x: 0, y: 0})
 
   createEffect(async () => {
     await invoke('start_recording')
-    const unlisten = await listen<{src: string}>('frame', (event) => {
+    // const mediaStream = startCapture({ video: true, audio: false })
+    const unlisten = await listen<MouseLocation>('mouse_location', (event) => {
+      // console.log(event)
       // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
       // event.payload is the payload object
-      setCurrentFrame(event.payload.src);
+      setMouseLocation(event.payload);
     })
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
@@ -25,7 +43,13 @@ function App() {
   
   return (
     <div class="container">
-      {currentFrame() ? <img src={currentFrame()} /> : <h1>Waiting for first frame...</h1>}
+      {/* {currentFrame() ? <img src={currentFrame()} /> : <h1>Waiting for first frame...</h1>} */}
+      <h1>
+        
+        {`Mouse Location: ${mouseLocation()?.x}, ${mouseLocation()?.y}`}
+        
+      </h1>
+
     </div>
   );
 }
